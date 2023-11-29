23e5d60d2532e33b9906770fef76d37a975e8433
quo0001 <alex.jarrah1@gmail.com>
2023-11-29 03:41:57 -0500

    Added notifier once monitoring has started

--- internal/discord/start.go
+++ internal/discord/start.go
@@ -1,6 +1,8 @@
 package discord
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/quo0001/Discord-Telegram-Mirror/internal"
 )
@@ -16,6 +18,8 @@ func Start() error {
 	// Specify required intents for the session
 	dg.Identify.Intents = discordgo.IntentsAll
 
+	// Add a ready handler to notify once monitoring has started
+	dg.AddHandler(ready)
 	// Add a message creation handler
 	dg.AddHandler(messageCreate)
 
@@ -27,4 +31,9 @@ func Start() error {
 
 	// Wait for signals to exit
 	select {}
+}
+
+// Notifies the user once monitoring has started
+func ready(s *discordgo.Session, m *discordgo.Ready) {
+	fmt.Printf("Monitor running on %s... Press Ctrl-C to exit\n", s.State.User.Username)
 }
